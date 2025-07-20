74e9d63e014bbc1d31a8563a253c341e097eddbf
Jossel Itto Marquez <itto@pogi.dev>
2025-07-20 13:04:09 +0800

    restructure pages for new pages preps

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import '@/app/globals.css';
+import Footer from '@/app/components/Footer';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'pogi.dev',
+  description: 'Under Construction',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+      <h1 className='text-5xl font-extrabold'>Hello World!</h1>
+      <p>
+        Page under construction. Come back in a few seconds, maybe something has
+        changed (or not).
+      </p>
+    </main>
+  );
+}
